71282071daf0b3fa050d5105df23567d76c40fb4
minaolensarah <s.b1999@gmx.de>
2022-05-10 19:50:22 +0300

    add datasets import and gitignore

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -4,6 +4,17 @@ import pandas as pd
 from tensorflow_datasets.summarization.scientific_papers import ScientificPapers
 
 if __name__ == "__main__":
+
+    #pip install datasets
+from datasets import load_dataset
+
+dataset = load_dataset("ccdv/arxiv-summarization")
+print(dataset)
+
+with open("sum_data.pkl", "wb" ) as p:
+    pickle.dump(dataset, p)
+"""
+if __name__ == "__main__":
     builder = tfds.builder('scientific_papers')
     builder.download_and_prepare()
 # 2. Load the `tf.data.Dataset`
@@ -12,6 +23,7 @@ if __name__ == "__main__":
  #   ds = tfds.load('scientific_papers', split='train', shuffle_files=True)
    # arxiv = ScientificPapers()
    # dataset_dataframe = tfds.as_dataframe()
+"""
 
 
 
